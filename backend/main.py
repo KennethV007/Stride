56c5c241e6005ce75260e2171009e81093591da2
Aiden Letourneau <aidenletourneau@gmail.com>
2025-06-28 10:37:35 -0400

    supabase test

--- backend/main.py
+++ backend/main.py
@@ -1,1 +1,10 @@
-print("baka")
+import dotenv
+import os
+from supabase import create_client, Client
+
+
+if __name__ == "__main__":
+    dotenv.load_dotenv()
+
+    supabase: Client = create_client(os.getenv("SUPABASE_URL"), os.getenv("SUPABASE_KEY"))
+
